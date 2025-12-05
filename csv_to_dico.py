4be07d8af907f74f3480c7b38524cb6dacb6c526
arthur magron <89658602+magarthur@users.noreply.github.com>
2025-12-05 22:17:00 +0100

    Fix indentation in csv_to_dico function

--- csv_to_dico.py
+++ csv_to_dico.py
@@ -1,3 +1,6 @@
 def csv_to_dico(csv,theme_sets):
 	for e in csv['theme'].unique():
-    	theme_sets[e] = csv[csv['theme'] == e].iloc[:, [i for i in range(csv.shape[1]) if i != 552]]
+		theme_sets[e] = csv[csv['theme'] == e].iloc[:, [i for i in range(csv.shape[1]) if i != 552]]
+		
+    	
+
