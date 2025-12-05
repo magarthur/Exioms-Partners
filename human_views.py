400757f44f6ac6256e481c3692b2e93cfdcc3128
arthur magron <89658602+magarthur@users.noreply.github.com>
2025-12-06 00:34:37 +0100

    Add files via upload

---
+++ human_views.py
@@ -0,0 +1,18 @@
+def human_views(train, date_cols):
+	df_clean = train.drop_duplicates(subset=['page_name', 'lang', 'agent']) # On s'assure ici qu'il n'y a pas de doublon
+	df_clean = df_clean.set_index(['page_name', 'lang'])	# On transforme l'index en étiquette, permet d'eviter des erreurs de décalage
+
+	df_all = df_clean.loc[df_clean['agent'] == 'all-agents', date_cols]	# on sépare ici robots et humains
+	df_spider = df_clean.loc[df_clean['agent'] == 'spider', date_cols]
+
+	df_human = df_all.sub(df_spider, fill_value=0) # on soustrait df_spider à df_all
+
+	meta_cols = ['Page', 'project', 'access']	# Permet de rajouter les différentes infos de la page au dataset final
+	df_meta = df_clean.loc[df_clean['agent'] == 'all-agents', meta_cols]
+
+	train_human_views = pd.concat([df_meta, df_human], axis=1).reset_index()	#on concatène le tout puis on redéfinit un index.
+
+	return train_human_views
+
+
+
