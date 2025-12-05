e16dd8d515d6f06b62daf3edb418c64dda650ba3
arthur magron <89658602+magarthur@users.noreply.github.com>
2025-12-06 00:14:32 +0100

    Enhance bot_and_human function with comments
    Added comments to clarify the purpose of the function and its filtering process.

--- bot_and_human.py
+++ bot_and_human.py
@@ -1,10 +1,17 @@
+
+# fonction permettant de sélectionner toutes les lignes qui possèdent all-access, spider et all-agents
 def bot_and_human(train):
 	train_all_access = train[train['access']=='all-access'].copy()
 	agents_per_page = train_all_access.groupby(['page_name','lang'])['agent'].unique()
 
 	valid_pairs = agents_per_page[agents_per_page.apply(lambda x: set(['all-agents', 'spider']).issubset(set(x)))].index
+	# On sélectionne les couples (page_name, lang) qui possèdent à la fois :
+	#- 'all-agents'
+	#- 'spider' que fait cette fonction ?
 
 	train_clean = train_all_access.set_index(['page_name', 'lang']).loc[valid_pairs].reset_index()
+	# on filtre le dataset pour ne garder que les lignes correspondant aux couples (page_name, lang) identifiées comme complets
 	return train_clean
 
 	
+
