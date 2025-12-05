32047f72aba05668752bddac62dcfd01adf43719
arthur magron <89658602+magarthur@users.noreply.github.com>
2025-12-05 22:41:56 +0100

    Add date_cols parameter to sum_views function

--- sum_views.py
+++ sum_views.py
@@ -1,9 +1,10 @@
 
-#Cette fonction prend en paramètre un dictionnaire dont les clés sont les thèmes et les valeurs sont les noms des pages accompagnés de leurs vues journalières.
+#Cette fonction prend en paramètre un dictionnaire dont les clés sont les thèmes et les valeurs sont les noms des pages accompagnés de leurs vues journalières. Et une liste de toutes les dates du dataset
 #Elle retourne un dictionnaire dont les clefs sont les thèmes et les valeurs correspondent à la moyenne des vues par jour (par thème).
-def sum_views(theme_sets):
+def sum_views(theme_sets,date_cols):
 	sums_themes = {}
 	for key in theme_sets:
 		sums_themes[key] = theme_sets[key][date_cols].sum(axis=0)/theme_sets[key].shape[0]
 	return sums_themes
+
 
