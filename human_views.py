624c5f52b2d8bc49c16f29b711b18eab65def6d9
arthur magron <89658602+magarthur@users.noreply.github.com>
2025-12-11 16:34:44 +0100

    Update human_views.py

--- human_views.py
+++ human_views.py
@@ -7,14 +7,10 @@ def human_views(train, date_cols):
 	df_all = df_clean.loc[df_clean['agent'] == 'all-agents', date_cols]	# on sépare ici robots et humains
 	df_spider = df_clean.loc[df_clean['agent'] == 'spider', date_cols]
 
-	df_human = df_all.sub(df_spider, fill_value=0) # on soustrait df_spider à df_all
-
-	meta_cols = ['Page', 'project', 'access']	# Permet de rajouter les différentes infos de la page au dataset final
-	df_meta = df_clean.loc[df_clean['agent'] == 'all-agents', meta_cols]
-
-	train_human_views = pd.concat([df_meta, df_human], axis=1).reset_index()	#on concatène le tout puis on redéfinit un index.
+	df_human = df_all.sub(df_spider, fill_value=0).reset_index() # on soustrait df_spider à df_all
 
 	return train_human_views
+
 
 
 
